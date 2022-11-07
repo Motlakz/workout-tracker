2add70b88b13d0828fd56aa2e1cdfd5b9170f53c
Motlakz <motlalepulasello5@gmail.com>
2022-11-07 13:08:15 +0200

    main: update table width

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ static LOCAL_STORAGE_DATA_KEY = "workout-tracker-entries";
         });
     }
     static html() {
-        return `<table class="tracker" style="width: 100%;">
+        return `<table class="tracker" style="width: 100vw;">
         <thead>
             <tr>
                 <th>Date</th>
